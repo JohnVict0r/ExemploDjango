1ecec3233785053002e2829966aa3ae24bf3907e
John Victor <john@iMac-de-LAIS-IS-6.local>
2019-09-17 16:59:24 -0300

    django rest framework

---
+++ todo/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from todo.models import TodoItem
+
+class TodoItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TodoItem
+        fields = ['id', 'name', 'done']
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+from todo.models import TodoItem
+from todo.serializers import TodoItemSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    serializer_class = TodoItemSerializer
+    queryset = TodoItem.objects.all()
